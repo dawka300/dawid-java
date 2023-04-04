bc7a016db6bf95d96dc36c414442e43aad008e7b
dwesolowski <dawid.wesolowski@silkycoders.com>
2023-04-04 22:42:03 +0200

    git init

---
+++ src/main/java/silkyjava/dawidjava/DawidJavaApplication.java
@@ -0,0 +1,13 @@
+package silkyjava.dawidjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawidJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawidJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silkyjava/dawidjava/DawidJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package silkyjava.dawidjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawidJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
